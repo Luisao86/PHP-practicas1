9e35e4962a929214e7971a7891abc715b25f88af
Luis Funes <Luis.Funes86@hotmail.com>
2023-01-30 23:29:08 -0300

    se agregaron funciones en admin/productos.

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,11 +1,81 @@
 <?php include("../template/cabecera.php"); ?>
 
+<?php 
+
+$txtid=(isset($_POST['txtID']))?($_POST['txtID']):"";
+$txtNombre=(isset($_POST['txtNombre']))?($_POST['txtNombre']):"";
+$txtImagen=(isset($_FILES['txtImagen']['name']))?($_FILES['txtImagen']['name']):"";
+$accion=(isset($_POST['accion']))?($_POST['accion']):"";
+
+echo $txtid."<br/>";
+echo $txtNombre."<br/>";
+echo $txtImagen."<br/>";
+echo $accion."<br/>";
+
+
+?>
+
+
+
 <div class="col-md-5">
-    Formulario para agregar los productos
+
+    <div class="card">
+        <div class="card-header">
+            Datos del producto
+        </div>
+        <div class="card-body">
+            <form method="POST" enctype="multipart/form-data">
+
+            <div class = "form-group">
+                <label for="exampleInputEmail1">ID:</label>
+                <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+            </div>
+
+            <div class = "form-group">
+                <label for="exampleInputEmail1">Nombre:</label>
+                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del producto">
+            </div>
+
+            <div class = "form-group">
+                <label for="exampleInputEmail1">Imagen:</label>
+                <input type="file" name="txtImagen" id="txtImagen" placeholder="Imagen">
+            </div>
+
+            <div class="btn-group" role="group" aria-label="">
+                <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar producto</button>
+                <button type="submit" name="accion" value="Modificar" class="btn btn-warning">Modificar producto</button>
+                <button type="submit" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
+            </div>
+</form>
+        </div>
+    </div>
+
+   
+    
+    
 </div>
 
 <div class="col-md-7">
-    tabla de productos(vamos a mostrar los datos de los productos)
+    
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Imagen</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>3</td>
+                <td>sistema PHP</td>
+                <td>Imagen.jpg</td>
+                <td>Seleccionar | Borrar </td>
+            </tr>
+        </tbody>
+    </table>
+
 </div>
 
 
