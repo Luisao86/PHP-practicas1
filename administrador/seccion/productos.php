b7b5d7af08e87fefc878a00ebd3d16098d47fd64
Luis Funes <Luis.Funes86@hotmail.com>
2023-02-02 22:57:05 -0300

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -13,21 +13,66 @@ include("../config/db.php");
 
 switch ($accion) {
     case 'Agregar':
-        $sentenciSQL= $conexion->prepare("INSERT INTO productos (nombre, imagen) VALUES (:nombre,:imagen);");
-        $sentenciSQL->bindParam(':nombre',$txtNombre);
-        $sentenciSQL->bindParam(':imagen',$txtImagen);
-        $sentenciSQL->execute();
+        $sentenciaSQL= $conexion->prepare("INSERT INTO productos (nombre, imagen) VALUES (:nombre,:imagen);");
+        $sentenciaSQL->bindParam(':nombre',$txtNombre);
+
+        $fecha= new DateTime();
+        $nombreArchivo=($txtImagen!="")?$fecha->getTimestamp()."_".$_FILES['txtImagen']['nombre']:"imagen.jpg";
+
+        $tmpimagen=$_FILES["txtImagen"]["tmp_imagen"];
+
+        if($tmpimagen)
+
+        $sentenciaSQL->bindParam(':imagen',$txtImagen);
+        $sentenciaSQL->execute();
         break;
 
     case 'Modificar':
-        echo "Presionado boton modificar";
+
+        $sentenciaSQL= $conexion->prepare("UPDATE productos SET nombre=:nombre WHERE ID=:ID");
+        $sentenciaSQL->bindParam(':nombre',$txtNombre);
+        $sentenciaSQL->bindParam(':ID',$txtid);
+        $sentenciaSQL->execute();
+
+        if($txtImagen!=""){ 
+            
+            $sentenciaSQL= $conexion->prepare("UPDATE productos SET imagen=:imagen WHERE ID=:ID");
+            $sentenciaSQL->bindParam(':imagen',$txtImagen);
+            $sentenciaSQL->bindParam(':ID',$txtid);
+            $sentenciaSQL->execute();
+        }
+
         break;
 
     case 'Cancelar':
         echo "Presionado boton cancelar";
         break;
+
+    case 'Seleccionar':
+
+        $sentenciaSQL= $conexion->prepare("SELECT * FROM productos WHERE ID=:ID");
+        $sentenciaSQL->bindParam(':ID',$txtid);
+        $sentenciaSQL->execute();
+        $producto=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
+
+        $txtNombre=$producto['nombre'];
+        $txtImagen=$producto['imagen'];
+
+
+        //echo "Presionado boton Seleccionar";
+        break;
+
+    case 'Borrar':
+       // echo "Presionado boton Borrar";
+        $sentenciaSQL= $conexion->prepare("DELETE FROM productos WHERE ID=:ID");
+        $sentenciaSQL->bindParam(':ID',$txtid);
+        $sentenciaSQL->execute();
+        break;
 }
 
+    $sentenciaSQL= $conexion->prepare("SELECT * FROM productos");
+    $sentenciaSQL->execute();
+    $listaProductos=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
@@ -43,16 +88,17 @@ switch ($accion) {
 
             <div class = "form-group">
                 <label for="exampleInputEmail1">ID:</label>
-                <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+                <input type="text" class="form-control" value="<?php echo $txtid;?>" name="txtID" id="txtID" placeholder="ID">
             </div>
 
             <div class = "form-group">
                 <label for="exampleInputEmail1">Nombre:</label>
-                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del producto">
+                <input type="text" class="form-control" value="<?php echo $txtNombre;?>" name="txtNombre" id="txtNombre" placeholder="Nombre del producto">
             </div>
 
             <div class = "form-group">
                 <label for="exampleInputEmail1">Imagen:</label>
+                <?php echo $txtImagen;?>"
                 <input type="file" name="txtImagen" id="txtImagen" placeholder="Imagen">
             </div>
 
@@ -82,12 +128,27 @@ switch ($accion) {
             </tr>
         </thead>
         <tbody>
+            <?php foreach($listaProductos as $producto) { ?> 
             <tr>
-                <td>3</td>
-                <td>sistema PHP</td>
-                <td>Imagen.jpg</td>
-                <td>Seleccionar | Borrar </td>
+                <td><?php echo $producto['ID']?></td>
+                <td><?php echo $producto['nombre']?></td>
+                <td><?php echo $producto['imagen']?></td>
+                
+                <td>
+                    
+                    <form method="POST">
+                        
+                        
+                        <input type="hidden" name="txtID" id="txtID" value="<?php echo $producto['ID'];?>">
+                        
+                        <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary">
+                        <input type="submit" name="accion" value="Borrar" class="btn btn-danger">
+
+                    </form>
+                           
+                </td>
             </tr>
+            <?php }?>
         </tbody>
     </table>
 
